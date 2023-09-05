047321e62e52b14a1084e1d0a890f1c00b299b1a
Cheng Tan <c.tan@northeastern.edu>
2023-09-05 08:46:31 -0400

    env.sh

---
+++ env.sh
@@ -0,0 +1,15 @@
+export EGOS=$(pwd)
+
+# lab1-ex1:
+
+# # if you use MacOS, you should uncomment the following two lines:
+# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-apple-darwin/bin
+# export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-apple-darwin/bin
+
+# # if you use Ubuntu, you should uncomment the following two lines:
+# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin
+# export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-linux-ubuntu14/bin
+
+# # if you're using other OSes, please fill in XXX below
+# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-XXX/bin
+# export PATH=$PATH:$EGOS/riscv-qemu-XXX/bin
